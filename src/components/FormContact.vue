e66f373feea740f3fb3674db214054e4e0c6b7d2
Draxx0 <williamfort.lmgl@gmail.com>
2022-06-02 11:00:55 +0200

    first commit

---
+++ src/components/FormContact.vue
@@ -0,0 +1,51 @@
+<template>
+  <form action="">
+    <div class="form-group">
+      <label for="name">Name</label>
+      <input
+        type="text"
+        class="form-control"
+        id="name"
+        placeholder="Enter name"
+      />
+    </div>
+    <div class="form-group">
+      <label for="email">Email</label>
+      <input
+        type="email"
+        class="form-control"
+        id="email"
+        placeholder="Enter email"
+      />
+    </div>
+    <div class="form-group">
+      <label for="message">Message</label>
+      <textarea class="form-control" id="message" rows="3"></textarea>
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
+</template>
+
+<style>
+form {
+  max-width: 500px;
+  margin: 0 auto;
+}
+
+.form-group {
+  margin-bottom: 1rem;
+}
+
+.form-group label {
+  display: block;
+  margin-bottom: 0.5rem;
+}
+
+textarea {
+  resize: none;
+}
+</style>
+
+<script>
+    
+</script>
